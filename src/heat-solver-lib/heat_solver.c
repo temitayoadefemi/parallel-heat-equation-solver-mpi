d4491ab2de34d6cb5ec2b5dde8a35a9d7f82676d
temitayoadefemi <111149469+temitayoadefemi@users.noreply.github.com>
2024-09-02 10:22:29 +0100

    Update heat_solver.c

--- src/heat-solver-lib/heat_solver.c
+++ src/heat-solver-lib/heat_solver.c
@@ -4,10 +4,10 @@
 #include "structs.h"
 #include "misc.h"
 #include <stdbool.h>
-#include <math.h>
+#include <math.h> // Ensure math.h is included for math functions
 
-slc_str get_serial_dims(master_str *master)
-{
+// Function to get serial dimensions
+slc_str get_serial_dims(master_str *master) {
     slc_str slice;
 
     slice.actual.width = master->params.landscape;
@@ -25,10 +25,11 @@ slc_str get_serial_dims(master_str *master)
     return slice;
 }
 
-slc_str get_parallel_dims(master_str *master)
-{
+// Function to get parallel dimensions
+slc_str get_parallel_dims(master_str *master) {
     slc_str slice;
 
+    // Ensure cart is defined or passed as a parameter
     slice.actual.width  = (int) floor((double)master->params.landscape  / (double)cart.dims[0]);
     slice.actual.height = (int) floor((double)master->params.landscape / (double)cart.dims[1]);
 
@@ -50,7 +51,8 @@ slc_str get_parallel_dims(master_str *master)
     return slice;
 }
 
-void initialize_cell_buffers(**double values, **double levels, dim_str dimensions ) {
+// Function to initialize cell buffers
+void initialize_cell_buffers(double **values, double **levels, dim_str dimensions) {
     // Add bounds checking in loops
     for (int i = 0; i < dimensions.width; i++) {
         for (int j = 0; j < dimensions.height; j++) {
@@ -66,47 +68,81 @@ void initialize_cell_buffers(**double values, **double levels, dim_str dimension
     }
 }
 
+// Function to initialize dt
 void initialize_dt(double *dt, double dx, double dy) {
     *dt = 0.25 * (dx * dx + dy * dy); 
 }
 
-
-
-void solve_heat_equation(**double values, double dx, double dy, double dt)  {
-    for (int i = 1; i < local_lx - 1; i++) {
-        for (int j = 1; j < local_ly - 1; j++) {
+// Function to solve the heat equation
+void solve_heat_equation(double **values, double dx, double dy, double dt, slc_str slice)  {
+    // Ensure local_lx and local_ly are defined or passed as parameters
+    for (int i = 1; i < slice.actual.width - 1; i++) {
+        for (int j = 1; j < slice.actual.height - 1; j++) {
             double laplacian_value = laplacian(values, i, j, dx, dy);
+            // Correct array access
             values[i][j] += dt * laplacian_value;
         }
     }
 }
 
-
-void refine_mesh(**double levels, **double values, double dx, double dy) {
-    for (int i = 1; i < local_lx - 1; i++) {
-        for (int j = 1; j < local_ly - 1; j++) {
+// Function to refine the mesh
+void refine_mesh(double **levels, double **values, double dx, double dy, slc_str slice) {
+    // Ensure local_lx, local_ly, ca, and MAX_LEVEL are defined or passed as parameters
+    for (int i = 1; i < slice.actual.width - 1; i++) {
+        for (int j = 1; j < slice.actual.height - 1; j++) {
             if (ca[i][j].level < MAX_LEVEL && fabs(laplacian(values, i, j, dx, dy)) > 0.1) {
                 levels[i][j]++;
-                update_neighbors(i, j, local_grid);
+                update_neighbors(i, j, levels);
             }
         }
     }
 }
 
+// Function to distribute cells
+void distribute_cells(double **local, double **global, cart_str cart, slc_str slice) {
+    for (int i = 0; i < slice.actual.width; i++) {
+        for (int j = 0; j < slice.actual.height; j++) {
+            // Ensure master and its dimensions are defined or passed as parameters
+            int global_row = cart.coords[0] * master->dimensions.rows + i;
+            int global_col = cart.coords[1] * master->dimensions.cols + j;
+            local[i][j] = global[global_row][global_col];
+        }
+    }
+}
 
-void update_neighbors(int i, int j, **double levels) {
+// Function to zero out temporary cell buffer
+void zerotmpcell(double **temp, dim_str dimensions) {
+    for (int i = 0; i < dimensions.width; i++) {
+        for (int j = 0; j < dimensions.height; j++) {
+            temp[i][j] = 0;
+        }
+    }
+}
+
+// Function to copy buffer to mini
+void copy_buff_to_mini(double **mini, double **local, slc_str slice) {
+    for (int i = 1; i <= slice.actual.width; i++) { // Removed extra comma
+        for (int j = 1; j <= slice.actual.height; j++) {
+            mini[i - 1][j - 1] = local[i][j];
+        }
+    }
+}
+
+// Function to update neighbors
+void update_neighbors(int i, int j, double **levels) {
     int neighbors[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     for (int k = 0; k < 4; k++) {
         int ni = i + neighbors[k][0];
         int nj = j + neighbors[k][1];
+        // Ensure LX and LY are defined or passed as parameters
         if (ni >= 0 && ni < LX && nj >= 0 && nj < LY) {
            levels[ni][nj] = fmax(levels[ni][nj], levels[i][j] - 1);
         }
     }
 }
 
-
-double laplacian(**double values, int i, int j, double dx, double dy) {
+// Function to calculate the laplacian
+double laplacian(double **values, int i, int j, double dx, double dy) {
     return (values[i+1][j] + values[i-1][j] - 2*values[i][j]) / (dx*dx) +
            (values[i][j+1] + values[i][j-1] - 2*values[i][j]) / (dy*dy);
 }
