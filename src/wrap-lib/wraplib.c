9fa25dcf0d12a62a65045c31f0445c282b3a8788
temitayoadefemi <111149469+temitayoadefemi@users.noreply.github.com>
2024-09-02 18:38:04 +0100

    Update wraplib.c

--- src/wrap-lib/wraplib.c
+++ src/wrap-lib/wraplib.c
@@ -7,27 +7,7 @@
 #include "serlib.h"
 #include "parlib.h"
 
-int get_version(int an, char **av)
-{
-	int ai=1;
-	int version=serial;	//default
-	while(ai<an)
-	{
-		if(!strcmp(av[ai],"-ver"))
-		{	
-			if(atoi(av[ai+1])==0)
-			{
-				version = serial;
-			}else
-			{
-				version = par2D;
-			}
-		}
-		ai+=1;
-	}
 
-	return version;	
-}
 
 void setup_comm(master_str *master)
 {
@@ -37,27 +17,6 @@ void setup_comm(master_str *master)
 	}
 }
 
-void read_args(int an, char **av, master_str *master)
-{
-	if(master->params.version == serial)
-	{
-		serial_read_args(an,av,master);
-	}else if(master->params.version == par2D)
-	{
-		par_read_args(an,av,master);
-	}
-}
-
-void read_distribute_data(master_str *master)
-{
-	if(master->params.version == serial)
-	{
-		serial_read_data(master);
-	}else if(master->params.version == par2D)
-	{
-		par_read_scatter_data(master);
-	}
-}
 
 void setup_buffers(master_str *master)
 {
@@ -88,6 +47,7 @@ void stop_timing(master_str *master)
 	if(master->params.version == serial)
 	{
 		serial_stop_timing(master);
+        
 	}else if(master->params.version == par2D)
 	{
 		par_stop_timing(master);
@@ -99,7 +59,7 @@ void process(master_str *master)
 	if(master->params.version == serial)
 	{
 		serial_process(master);
-        
+
 	}else if(master->params.version == par2D)
 	{
 		par_process(master);
@@ -111,6 +71,7 @@ void gather_write_data(master_str *master)
 	if(master->params.version == serial)
 	{
 		serial_write_data(master);
+
 	}else if(master->params.version == par2D)
 	{
 		par_gather_write_data(master);
@@ -122,6 +83,7 @@ void clean_buffers_end_comm(master_str *master)
 	if(master->params.version == serial)
 	{
 		serial_clean_buffers(master);
+
 	}else if(master->params.version == par2D)
 	{
 		par_clean_buffers_stop_comm(master);
