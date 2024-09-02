d3789a88936b4925fcc9ba17cb493921fdcffc91
temitayoadefemi <111149469+temitayoadefemi@users.noreply.github.com>
2024-09-02 09:52:54 +0100

    Update heat_solver.h

--- src/heat-solver-lib/heat_solver.h
+++ src/heat-solver-lib/heat_solver.h
@@ -4,12 +4,21 @@
 #include "structs.h"  // Including necessary structures like cart_str, master_str, etc.
 #include <stdbool.h>
 
+slc_str get_serial_dims(master_str *master);
 
-void solve_heat_equation(cell **local_grid, double dx, double dy, double dt);
-void refine_mesh(cell **local_grid, double dx, double dy);
-void update_neighbors(int i, int j, cell **local_grid);
-double laplacian(cell **local_grid, int i, int j, double dx, double dy);
-void initialize_grid(cell  **global_grid);
+slc_str get_parallel_dims(master_str *master);
+
+void initialize_cell_buffers(**double values, **double levels, dim_str dimensions);
+
 void initialize_dt(double *dt, double dx, double dy);
+
+void solve_heat_equation(**double values, double dx, double dy, double dt);
+
+void refine_mesh(**double levels, **double values, double dx, double dy);
+
+void update_neighbors(int i, int j, **double levels);
+
+double laplacian(**double values, int i, int j, double dx, double dy);
+
 
 #endif // HEATSOLVERLIB_H
