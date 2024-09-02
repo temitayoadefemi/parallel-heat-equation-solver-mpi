87a0adaf8cb650aeefab1480a2cf3bc8672a503f
temitayoadefemi <111149469+temitayoadefemi@users.noreply.github.com>
2024-09-02 19:07:21 +0100

    Update mpilib.h

--- src/mpi-lib/mpilib.h
+++ src/mpi-lib/mpilib.h
@@ -10,8 +10,8 @@
 void mpstart(comm_str *comm);
 void mpstop(void);
 void setup_cartesian_topology(comm_str *comm, cart_str *cart);
-void send_halos_2D(cart_str *cart, MPI_Datatype row_type, MPI_Datatype column_type, *cell local_grid);	
-void receive_halos_2D(cart_str *cart, MPI_Datatype row_type, MPI_Datatype column_type, *cell local_grid);
+void send_halos_2D(cart_str *cart, MPI_Datatype row_type, MPI_Datatype column_type, double **values, slc_str slice);
+void receive_halos_2D(cart_str *cart, MPI_Datatype row_type, MPI_Datatype column_type, double **values, slc_str slice);
 void complete_communication_2D(cart_str *cart);
 void initialize_mpi_types(MPI_Datatype *column_type, MPI_Datatype *row_type, master_str *master);
 void initialize_mpi_buffer(void **buffer, int *bsize, master_str *master);
