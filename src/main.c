ba8f2c3d6f9d991112febea8c521854b80f62999
temitayoadefemi <111149469+temitayoadefemi@users.noreply.github.com>
2024-09-03 10:43:42 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -9,21 +9,11 @@ int main(int argc, char **argv)
 {
 	master_str master;
 
-	master.params.version = get_version(argc,argv);
-
 	setup_comm(&master);
-
-	read_args(argc, argv, &master);
-
-	read_distribute_data(&master);
 
 	setup_buffers(&master);
 
-	start_timing(&master);
-
 	process(&master);
-
-	stop_timing(&master);
 
 	gather_write_data(&master);
 
