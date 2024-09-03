2c26252e19335956445ec0298fad9ad62925ad5f
temitayoadefemi <111149469+temitayoadefemi@users.noreply.github.com>
2024-09-03 10:44:08 +0100

    Update wraplib.h

--- src/wrap-lib/wraplib.h
+++ src/wrap-lib/wraplib.h
@@ -3,16 +3,10 @@
 
 #include "structs.h"
 
-int get_version(int an, char **av);
+
 void setup_comm(master_str *master);
-void read_args(int an, char **av, master_str *master);
-void read_distribute_data(master_str *master);
 void setup_buffers(master_str *master);
-
-void start_timing(master_str *master);
-void stop_timing(master_str *master);
 void process(master_str *master);
-
 void gather_write_data(master_str *master);
 void clean_buffers_end_comm(master_str *master);
 
