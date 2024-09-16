0b9b75df5bbd6768dbb36284329dbc08be1dd6e1
temitayoadefemi <111149469+temitayoadefemi@users.noreply.github.com>
2024-09-16 06:48:31 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,21 +2,26 @@
 #include <stdlib.h>
 #include <math.h>
 #include <mpi.h>
-
 #include "wraplib.h"
+#include "args.h"
+#include "mpilib.h"
+
 
 int main(int argc, char **argv)
 {
+
 	master_str master;
 
 	setup_comm(&master);
 
-    if (read_args(&master, argc, argv) == FAILED) {
+	if (read_parameters(&master, argc, argv) == FAILED) {
 
         mpstop(); // Stop the MPI environment
 
         return 0;
     }
+
+
 
 	setup_buffers(&master);
 
