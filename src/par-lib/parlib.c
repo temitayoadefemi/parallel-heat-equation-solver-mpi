ca12d3b25fe2491fe0cf7196072f26693518f678
temitayoadefemi <111149469+temitayoadefemi@users.noreply.github.com>
2024-09-16 05:26:23 +0100

    Update parlib.c

--- src/par-lib/parlib.c
+++ src/par-lib/parlib.c
@@ -3,11 +3,11 @@
 #include <math.h>
 
 #include "parlib.h"
-#include "args.h"
 #include "mem.h"
-#include "pgmio.h"
 #include "heat_solver.h"
 #include "mpilib.h"
+#include "misc.h"
+
 
 
 
@@ -24,29 +24,48 @@ void par_initialise_buffers(master_str *master)
 	 * old and new image arrays to 255(white)
 	 * and setup fixed sawtooth boundaries to left and right
 	 */
-    initialize_cell_buffers(&master->cell.buffers.global.values, &master->cell.buffers.global.levels, &master->dimensions);
-    distribute_cells(&master->cell.buffers.mini.values, &master->cell.buffers.global.values, &master->cart, &master->slice)
+    master->slice = get_parallel_dims(master->cart);
+    master->cell.buffers = allocate_parallel_buffers(master->slice, master->cart);
+    initialize_cell_buffers(master->cell.buffers.global.values, master->cell.buffers.global.levels, master->dimensions, master->cart);
+    mpbcast(master->cart, master->cell.buffers.global.values, master->slice.padded);
+    distribute_cells(master->cell.buffers.mini.values, master->cell.buffers.global.values, master->cart, master->slice);
+    copy_buff_to_local(master->cell.buffers.local.values, master->cell.buffers.local.levels, master->cell.buffers.global.values, master->cell.buffers.global.levels, master->slice);
+    initialise_edges(master->cell.buffers.local.values, master->cell.buffers.local.levels, master->slice.actual);
+
 }
 
-void par_halo_exchange(master_str *master)
-{
+void par_halo_exchange(master_str *master) {
+    MPI_Datatype column_type, row_type;
+    void *buffer;
+    int bsize;
 
-    send_halos_2D(&master->cart, row_type, column_type);
-    receive_halos_2D(&master->cart, hor_halo_type, &master->img, master->slice);     
+    initialize_mpi_types(&column_type, &row_type, master->slice);
+    initialize_mpi_buffer(&buffer, &bsize, master->slice);
+
+    send_halos_2D(&master->cart, row_type, column_type, master->cell.buffers.local.values, master->slice);
+    receive_halos_2D(&master->cart, row_type, column_type, master->cell.buffers.local.values, master->slice);     
     complete_communication_2D(&master->cart);
 
 }
 
 void par_process(master_str *master) {
 
-	 for (int step = 0; step < 1000; step++) {
-        
+    double dt = 0.25 * (1.0 / master->slice.actual.width * 1.0 / master->slice.actual.width + 1.0 / master->slice.actual.height * 1.0 / master->slice.actual.height);
+
+    for (int step = 0; step < 1000; step++) {
+
         par_halo_exchange(master);
-        solve_heat_equation(&master->cell.buffers.local.values, dx, dy, dt, &master->slice);
+        
+        solve_heat_equation(master->cell.buffers.local.values, 
+                            1.0 / master->slice.actual.width, 
+                            1.0 / master->slice.actual.height, 
+                            dt, master->slice);
 
         if (step % 10 == 0) {
-
-            refine_mesh(&master->cell.buffers.local.levels, &master->cell.buffers.local.values, dx, dy)
+            refine_mesh(master->cell.buffers.local.levels, 
+                        master->cell.buffers.local.values, 
+                        1.0 / master->slice.actual.width, 
+                        1.0 / master->slice.actual.height, master->slice, 3);
         }
     }
 }
@@ -54,19 +73,18 @@ void par_process(master_str *master) {
 
 void par_gather_write_data(master_str *master)
 {
-    copy_buff_to_mini(&master->cell.buffers.mini.values, &master->cell.buffers.local.values, &master->slice);
-    zerotmpcell(&master->cell.buffers.temp.values, &master->dimensions);
-    distribute_cells(&master->cell.buffers.local.values, &master->cell.buffers.global.values, &master->cart, &master->slice);
-    mpireduce(&master->cart, &master->cell.buffers.temp.values, &master->cell.buffers.global.values, &master->dimensions.width*&master->dimensions.height);
-
+    copy_buff_to_mini(master->cell.buffers.mini.values, master->cell.buffers.local.values, master->slice);
+    zerotmpcell(master->cell.buffers.temp.values, master->cell.buffers.temp.levels , master->dimensions);
+    distribute_cells(master->cell.buffers.local.values, master->cell.buffers.global.values, master->cart, master->slice);
+    mpireduce(master->cart, master->cell.buffers.temp.values, master->cell.buffers.global.values, master->slice.padded);
 
     if (master->comm.rank == 0) {
-        save_to_pbm("heat_equation", global_grid);
+        writecelldynamic("heat equation", master->cell.buffers.global.values, 1152);
     }
 }
 
 void par_clean_buffers_stop_comm(master_str *master)
 {
-	dealocate_buffers(&master->img.buffers);
+	dealocate_buffers(&master->cell.buffers);
 	mpstop();
 }
